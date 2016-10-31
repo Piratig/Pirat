9113152fb364acb59a92fb673556ee1dfc962337
Piratig <his1994@mail.ru>
2016-10-31 13:18:01 +0300

    Написан генератор.

--- Password Generator/Password Generator/Form1.Designer.cs
+++ Password Generator/Password Generator/Form1.Designer.cs
@@ -36,7 +36,18 @@
             this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiLoad = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.tclGenerator = new System.Windows.Forms.TabControl();
+            this.tbpGenerator = new System.Windows.Forms.TabPage();
+            this.tclNotepad = new System.Windows.Forms.TabPage();
+            this.chlbGenerator = new System.Windows.Forms.CheckedListBox();
+            this.nudLength = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerates = new System.Windows.Forms.Button();
+            this.txbPassword = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
+            this.tclGenerator.SuspendLayout();
+            this.tbpGenerator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -102,17 +113,120 @@
             this.tsmiAbout.Text = "About";
             this.tsmiAbout.Click += new System.EventHandler(this.tsmiAbout_Click);
             // 
+            // tclGenerator
+            // 
+            this.tclGenerator.Controls.Add(this.tbpGenerator);
+            this.tclGenerator.Controls.Add(this.tclNotepad);
+            this.tclGenerator.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tclGenerator.Location = new System.Drawing.Point(0, 24);
+            this.tclGenerator.Name = "tclGenerator";
+            this.tclGenerator.SelectedIndex = 0;
+            this.tclGenerator.Size = new System.Drawing.Size(292, 249);
+            this.tclGenerator.TabIndex = 1;
+            // 
+            // tbpGenerator
+            // 
+            this.tbpGenerator.Controls.Add(this.txbPassword);
+            this.tbpGenerator.Controls.Add(this.btnGenerates);
+            this.tbpGenerator.Controls.Add(this.label1);
+            this.tbpGenerator.Controls.Add(this.nudLength);
+            this.tbpGenerator.Controls.Add(this.chlbGenerator);
+            this.tbpGenerator.Location = new System.Drawing.Point(4, 22);
+            this.tbpGenerator.Name = "tbpGenerator";
+            this.tbpGenerator.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpGenerator.Size = new System.Drawing.Size(284, 223);
+            this.tbpGenerator.TabIndex = 0;
+            this.tbpGenerator.Text = "Generator";
+            this.tbpGenerator.UseVisualStyleBackColor = true;
+            // 
+            // tclNotepad
+            // 
+            this.tclNotepad.Location = new System.Drawing.Point(4, 22);
+            this.tclNotepad.Name = "tclNotepad";
+            this.tclNotepad.Padding = new System.Windows.Forms.Padding(3);
+            this.tclNotepad.Size = new System.Drawing.Size(284, 223);
+            this.tclNotepad.TabIndex = 1;
+            this.tclNotepad.Text = "Notepad";
+            this.tclNotepad.UseVisualStyleBackColor = true;
+            // 
+            // chlbGenerator
+            // 
+            this.chlbGenerator.CheckOnClick = true;
+            this.chlbGenerator.FormattingEnabled = true;
+            this.chlbGenerator.Items.AddRange(new object[] {
+            "Lowercase",
+            "Uppercase",
+            "Numbers",
+            "Special symbols"});
+            this.chlbGenerator.Location = new System.Drawing.Point(8, 6);
+            this.chlbGenerator.Name = "chlbGenerator";
+            this.chlbGenerator.Size = new System.Drawing.Size(268, 64);
+            this.chlbGenerator.TabIndex = 0;
+            // 
+            // nudLength
+            // 
+            this.nudLength.Location = new System.Drawing.Point(97, 81);
+            this.nudLength.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudLength.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.nudLength.Name = "nudLength";
+            this.nudLength.Size = new System.Drawing.Size(54, 20);
+            this.nudLength.TabIndex = 1;
+            this.nudLength.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Password length";
+            // 
+            // btnGenerates
+            // 
+            this.btnGenerates.Location = new System.Drawing.Point(11, 111);
+            this.btnGenerates.Name = "btnGenerates";
+            this.btnGenerates.Size = new System.Drawing.Size(140, 23);
+            this.btnGenerates.TabIndex = 3;
+            this.btnGenerates.Text = "Genarates password";
+            this.btnGenerates.UseVisualStyleBackColor = true;
+            this.btnGenerates.Click += new System.EventHandler(this.btnGenerates_Click);
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(11, 140);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(265, 20);
+            this.txbPassword.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.tclGenerator);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Password Generator";
+            this.Text = "tab";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.tclGenerator.ResumeLayout(false);
+            this.tbpGenerator.ResumeLayout(false);
+            this.tbpGenerator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,6 +242,14 @@
         private System.Windows.Forms.ToolStripMenuItem tsmiLoad;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+        private System.Windows.Forms.TabControl tclGenerator;
+        private System.Windows.Forms.TabPage tbpGenerator;
+        private System.Windows.Forms.CheckedListBox chlbGenerator;
+        private System.Windows.Forms.TabPage tclNotepad;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Button btnGenerates;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudLength;
     }
 }
 
--- Password Generator/Password Generator/Form1.cs
+++ Password Generator/Password Generator/Form1.cs
@@ -12,9 +12,13 @@ namespace Password_Generator
 {
     public partial class Form1 : Form
     {
+        Random rnd;
+        char[] specialsymbols_char = new char[] {'%','*',')','?','#','$','^','&','~' };
+
         public Form1()
         {
             InitializeComponent();
+            rnd = new Random();
         }
 
         private void tsmiExit_Click(object sender, EventArgs e)
@@ -25,6 +29,31 @@ namespace Password_Generator
         private void tsmiAbout_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Password Generator: Generates passwords and saves them. \n Author: Kharkovskiy Igor.");
+        }
+
+        private void btnGenerates_Click(object sender, EventArgs e)
+        {
+            if (chlbGenerator.CheckedItems.Count == 0) return;
+            string password = "";
+            for (int i = 0; i < nudLength.Value; i++)
+            {
+                int n = rnd.Next(0, chlbGenerator.CheckedItems.Count);
+                string s = chlbGenerator.CheckedItems[n].ToString();
+                switch (s)
+                {
+                    case "Lowercase": password += Convert.ToChar(rnd.Next(97,122));
+                        break;
+                    case "Uppercase": password += Convert.ToChar(rnd.Next(65, 90));
+                        break;
+                    case "Numbers": password += rnd.Next(10).ToString();
+                        break;
+                   
+                    default:
+                        password += specialsymbols_char[rnd.Next(specialsymbols_char.Length)];
+                            break;
+                }
+                txbPassword.Text = password;
+            }
         }
     }
 }
